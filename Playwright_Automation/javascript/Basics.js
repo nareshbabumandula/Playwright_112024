962ba572313f23ee7a054c948ca8b3c18b5741e6
[nareshbabumandula] <naresh223@gmail.com>
2024-11-11 13:20:35 +0530

    11-NOv-2024: Naresh: Added logic for arithmetic operations

--- Playwright_Automation/javascript/Basics.js
+++ Playwright_Automation/javascript/Basics.js
@@ -1,4 +1,10 @@
 let a=10;
 let b=20;
 let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=b/a;
 console.log("Addition of a and b is : " + c);
+console.log("Subtraction of a and b is : " + d);
+console.log("Multiplication of a and b is : " + e);
+console.log("Division of a and b is : " + f);
