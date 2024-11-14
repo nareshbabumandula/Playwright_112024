8782773537494599f3ea61c39d4629b3ff2d98cd
[nareshbabumandula] <naresh223@gmail.com>
2024-11-14 14:14:06 +0530

    SauceDemo Example

---
+++ Playwright_Automation/tests/BasicsScript.ts
@@ -0,0 +1,14 @@
+var a=10;
+var b=20;
+var a=100;
+var c=a+b;
+console.log(c);
+console.log(typeof(a));
+
+function multiplication(a,b) {
+    c=a*b;
+    return c;
+}
+
+console.log(multiplication(10,20));
+
---
+++ Playwright_Automation/tests/Saucedemo.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+// Playwright recognizes files ending with .spec.js, , .test.js, spec.ts or test.ts 
+test('Access Website', async ({ page }) => {
+    console.log('Website accessed');
+});
+
+test('Login', async ({ page }) => {
+    console.log('Logged into the application..!');
+});
+
+test('Search Product', async ({ page }) => {
+    console.log('Product searched successfully..!');
+});
+
+test('Logout', async ({ page }) => {
+    console.log('Logged out from the application..!');
+});
+
