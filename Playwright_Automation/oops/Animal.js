955c8c66a0bb2c30c6bac2ddf2473f11356f25ff
[nareshbabumandula] <naresh223@gmail.com>
2024-11-25 12:46:22 +0530

    OOPS: Class, Object and Inheritance

---
+++ Playwright_Automation/oops/Animal.js
@@ -0,0 +1,40 @@
+class Animal{
+    type="pet";
+
+    constructor(name){
+        this.name=name;
+    }
+
+    speak(){
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+class Dog extends Animal {
+
+    // Characteristics
+    age=5;
+    breed = "German Shepard";
+    
+    // Behavior
+    walk(){
+        console.log(`${this.name} can walk.`);
+    }
+
+    run(){
+        console.log(`${this.name} can run.`);
+    }
+
+}
+
+const dog = new Dog("Jimmy");
+dog.walk();
+dog.run();
+console.log(dog.age);
+console.log(dog.breed);
+
+dog.speak();
+console.log(dog.type);
+
+
+
