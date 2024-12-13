f70a110fea82409b113e04200dd7d4ffb5c0d5d4
[nareshbabumandula] <naresh223@gmail.com>
2024-12-13 16:59:07 +0530

    Playwright built-in locators

--- Playwright_Automation/tests/Locators.spec.js
+++ Playwright_Automation/tests/Locators.spec.js
@@ -29,5 +29,43 @@ test('Playwright Locators', async ({page}) => {
    await page.waitForTimeout(1000);
    await page.locator('//input[@name="email_to[]" and @value=2]').click();
    
-   await page.waitForTimeout(2000);
+   await page.waitForTimeout(1000);
+   // Select option from the dropdown box
+   await page.getByLabel('Drop Down Box:').selectOption('Third Option');
+   await page.waitForTimeout(4000);
 });
+
+
+test.only('Playwright built-in locators', async ({page}) => {
+   await page.goto('https://www.mycontactform.com/samples.php');
+  
+   // 1. page.getByLabel() --> To locate an object by its associated text
+   // Select option from the dropdown box
+   await page.getByLabel('Drop Down Box:').selectOption('Third Option');
+   await page.waitForTimeout(3000);
+
+   // 2. page.getByRole()) --> To locate by explicit and implicit accessibility attributes.
+   await page.getByRole('Link', {name:'Features'}).click(); 
+   await page.waitForTimeout(3000);
+
+   // 3. page.getByText() --> To locate by text content
+   await page.getByText('Pricing').click();
+   await page.waitForTimeout(3000);
+   await page.goBack();
+   await page.waitForTimeout(3000);
+   await page.getByText('Pric').click();
+   await page.waitForTimeout(3000);
+   const sampleForms = await page.getByText('Sample Forms', {exact:true});
+   console.log("Tab name is : " + await sampleForms.innerText());
+   sampleForms.click();
+   await page.goBack();
+   await page.waitForTimeout(3000);
+   //await page.getByText('/Sample|Forms/').click();
+
+   // 4. page.getByPlaceHolder() --> To locate a textbox by placeholder
+   await page.getByPlaceholder('First Name').fill('John');
+   await page.waitForTimeout(3000);
+});
+
+
+
