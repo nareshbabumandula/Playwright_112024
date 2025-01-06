b555e20ad9611d9155d872250f64857f95f115cd
[nareshbabumandula] <naresh223@gmail.com>
2025-01-06 13:34:30 +0530

    POM - 06-Jan-2025

--- Playwright_Automation/tests/SubmitSampleEmailContactForm.spec.ts
+++ Playwright_Automation/tests/SubmitSampleEmailContactForm.spec.ts
@@ -1,8 +1,9 @@
 import {test,expect} from '@playwright/test'
 import { SampleEmailContactFormsPage } from '../pages/SampleEmailContactFormsPage';
-
+import { RegistrationPage } from '../pages/RegistrationPage';
 
 let sampleEmailContactFormsPage: SampleEmailContactFormsPage;
+let registrationPage: RegistrationPage;
 
 test('Sample Email and Contact Forms', async ({page}) => {
     sampleEmailContactFormsPage = new SampleEmailContactFormsPage(page); 
@@ -10,4 +11,13 @@ test('Sample Email and Contact Forms', async ({page}) => {
     await sampleEmailContactFormsPage.selectSendTo("Customer");
     await sampleEmailContactFormsPage.enterSubject("Test Subject");
     await page.waitForTimeout(5000);
+
+
+    registrationPage = new RegistrationPage(page);
+    await registrationPage.navigateToCreateAccountPage('https://www.mycontactform.com/signup.php');
+    await registrationPage.enterName('Peterson');
+    await registrationPage.enterEmailAddress('peter@gmail.com');
+    await page.waitForTimeout(5000);
+
+
 });
